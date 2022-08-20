b44ad01d786a991f31389048a14007ba8168b861
linus mutugi mate <mlynux@gmail.com>
2022-08-20 14:06:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record){
+    for (let items of record){
+        if(items.result === "W"){
+            return items.year;
+        }
+    }
+}
+
+
+let Item = record.find(superbowlWin);
+   // if(record == "W"){
+        //return record.year;
+  //  } else{
+       // return undefined;
+   // }
+
+   
